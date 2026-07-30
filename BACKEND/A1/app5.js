38c2de1ecf9c02af00fea28ab778ee19e70f8c77
Luis Formigari <tazerlipe@gmail.com>
2026-07-30 16:57:31 -0300

    correção2

--- BACKEND/A1/app5.js
+++ BACKEND/A1/app5.js
@@ -2,14 +2,17 @@ const { questionFloat } = require("readline-sync");
 
 console.log("---[ Calculadora de IMC ]---");
 
-const Altura = questionFloat("Qual a sua altura (m)? ");
+const Altura = questionFloat("Qual a sua altura (metros)? ");
 const Peso = questionFloat("Qual o seu peso (kg)? ");
 
 const IMC = Peso / (Altura * Altura);
 
 console.log(
 
-    // aurinha
+
+
+
+
     "Seu peso:", Peso,
     "| Sua altura:", Altura,
     "| IMC:", IMC.toFixed(2)
