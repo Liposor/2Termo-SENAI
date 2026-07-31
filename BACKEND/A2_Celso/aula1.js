2d47281c2e640214313a22e0527aad264d919b58
luis formigari <tazerlipe@gmail.com>
2026-07-31 08:21:50 -0300

    aula 3 desafio 1 e 2

--- BACKEND/A2_Celso/aula1.js
+++ BACKEND/A2_Celso/aula1.js
@@ -3,3 +3,6 @@ console.log("Olá, mundo!");
 console.log("Estou começando a aprender JavaScript.");
 
 console.log("Esta é a disciplina de BackEnd 1.");
+
+// Criei outra para testar o alterar entre as ""
+console.log("LUIS")
--- BACKEND/A2_Celso/aula3.js
+++ BACKEND/A2_Celso/aula3.js
@@ -1,3 +1,4 @@
+// Exemplo que o Celso enviou:
 const numero1 = 20;
 const numero2 = 5;
 const soma = numero1 + numero2;
@@ -10,3 +11,25 @@ console.log("Subtração:", subtracao);
 console.log("Multiplicação:", multiplicacao);
 console.log("Divisão:", divisao);
 console.log("Resto da divisão:", resto);
+
+
+// Desafio 1:
+
+const n1 = 9
+const n2 = 8
+const n3 = 7
+
+const media = (n1 + n2 + n3) / 3;
+
+console.log("Média: ", media);
+
+
+const prod = "Teclado";
+const preco = 39;
+const qnt = 10;
+const Valor_Total = preco * qnt;
+
+console.log("-------------- Nota Fiscal --------------");
+console.log("  Produto: ", prod, "| Valor: ", preco);
+console.log("  Quantidade comprada: ", qnt);
+console.log("            Valor final: ", Valor_Total)
