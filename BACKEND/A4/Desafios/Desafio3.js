e7c99c0212e3581caac6d5d696e5f3cd946ca8f1
Luis Formigari <tazerlipe@gmail.com>
2026-08-07 14:20:09 -0300

    Desafio 3

--- BACKEND/A4/Desafios/Desafio3.js
+++ BACKEND/A4/Desafios/Desafio3.js
@@ -1,1 +1,12 @@
 const entrada = require('readline-sync');
+
+const gasolina = entrada.questionInt("Quanto esta o valor do litro da Gasolina? ")
+const Alcool = entrada.questionInt("Quanto esta o valor do litro do Alcool? ")
+
+const diferenca = Alcool / gasolina
+
+if (diferenca <= 0.7) {
+    console.log("Abasteça com ÁCOOL! Está mais barato!!")
+} else {
+    console.log("Abasteça com GASOLINA! Compensa MAIS!!")
+}
